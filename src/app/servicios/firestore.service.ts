f095a1f8acdd9828d12331dab9939c3a2ebd61a7
Minato <rickycamposa@gmail.com>
2025-06-05 12:29:42 -0600

    Instalacion de FireBase
    Listo para usarse

---
+++ src/app/servicios/firestore.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { Firestore, collection, addDoc } from '@angular/fire/firestore';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FirestoreService {
+  constructor(private firestore: Firestore) {}
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyAxUwVVZ8UZK42l_tU0fCY_h1i1Vg4RHo4",
+    authDomain: "gorillas-gym.firebaseapp.com",
+    projectId: "gorillas-gym",
+    storageBucket: "gorillas-gym.firebasestorage.app",
+    messagingSenderId: "669476958567",
+    appId: "1:669476958567:web:c69d8e9501640253293fc2",
+    measurementId: "G-6ZCCXH7QV0"
+  },
+};
