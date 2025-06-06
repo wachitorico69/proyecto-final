fab3c7ed5f6a6031f72b101cc3970cac0ca4913e
wachitorico69 <morenodario919@gmail.com>
2025-06-05 18:57:28 -0600

    formulario de registro

--- src/app/unete/unete.component.ts
+++ src/app/unete/unete.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-unete',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './unete.component.html',
   styleUrl: './unete.component.css'
 })
@@ -12,7 +12,60 @@ export class UneteComponent {
 
   constructor(private fb: FormBuilder) {
     this.registroForm = this.fb.group({
-      correo: ['', [Validators.required]]
-    })
+      nombre: ["", [Validators.required, Validators.minLength(5)]],
+      correo: ["", [Validators.required, this.emailValidator]],
+      password: ["", [Validators.required, Validators.minLength(8), Validators.maxLength(20), this.passwordValidator2]],
+      passwordRepeted: ["", [Validators.required]]
+    }, { validators: this.passwordValidator() });
   }
+
+  //Validar correo
+  public emailValidator(control: FormControl): { [key: string]: boolean } | null {
+    const emailRegexp: RegExp = /[@]/;
+    if (control.value && !emailRegexp.test(control.value)) {
+        return { invalidEmail: true };
+    }
+    return null;
+  }
+
+  //Validar contraseña
+public passwordValidator(): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: any } | null => {
+    const formGroup = control as FormGroup;
+    const password = formGroup.get('password')?.value;
+    const repeat_password = formGroup.get('passwordRepeted')?.value;
+
+    if (!password || !repeat_password || password !== repeat_password) {
+      return { isValid: false };
+    }
+    return null;
+  };
+}
+
+
+  public passwordValidator2(control: FormControl): { [key: string]: boolean } | null {
+    const passRegexp: RegExp = /[!@#$%^&*()+\-=\[\]{};':"\\|,.<>\/?]/;
+    const value = control.value;
+
+    //que no tenga caracteres especiales
+    if (value && passRegexp.test(value)) {
+        return { invalidPass: true };
+    }
+
+    //que tenga una mayuscula
+    const hasUpperCase: boolean = /[A-Z]/.test(value);
+    if (!hasUpperCase) {
+      return { noUpperCase: true };
+    }
+
+    //que tenga un numero al menos
+    const hasNumber: boolean = /[0-9]/.test(value);
+    if (!hasNumber) {
+     return { noNumber: true };
+    
+    }
+    return null;
+  }
+
+  submit(){}
 }
