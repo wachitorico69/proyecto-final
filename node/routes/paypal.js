c4211ecf0de0c7f9c1c202e9e82c8c9c1478f5c1
wachitorico69 <Emilianohdzvi@gmail.com>
2025-06-07 10:30:48 -0600

    directorios node organizados

---
+++ node/routes/paypal.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const router = express.Router();
+const paypal = require('../services/paypal');
+const nodemailer = require('nodemailer');
+
+let correoCompra = null;
+
+router.post('/pay', async (req, res) => {
+  const { correo } = req.body;
+  correoCompra = correo;
+
+  try {
+    const url = await paypal.createOrder();
+    res.json({ url });
+  } catch (error) {
+    console.error('Error en /pay:', error);
+    res.status(500).json({ error: 'Error al enviar correo o crear orden' });
+  }
+});
+
+router.get('/complete-order', async (req, res) => {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
+    }
+  });
+
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: correoCompra,
+    subject: 'Gracias por comprar en Gorilla Gym!',
+    text: 'Gracias por tu transacción reciente en Gorilla Gym.\nCurso Online\nTotal: Mex$ 500.00'
+  };
+
+  try {
+    const orderId = req.query.token;
+    await paypal.capturePayment(orderId);
+    await transporter.sendMail(mailOptions);
+    console.log(`Correo enviado a ${correoCompra}`);
+    res.redirect('http://localhost:4200/home');
+  } catch (error) {
+    console.error(error.response?.data || error);
+    res.send('Error ' + error);
+  }
+});
+
+router.get('/cancel-order', (req, res) => {
+  res.redirect('http://localhost:4200/clases');
+});
+
+module.exports = router;
