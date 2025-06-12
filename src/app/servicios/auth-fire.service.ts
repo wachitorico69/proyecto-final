e8830087a6010ca12c712bf635488f9184bc18c6
Chrissw1138 <chrissw38@outlook.es>
2025-06-12 11:33:49 -0600

    Cambio de contraseña y bloqueo de cuenta

---
+++ src/app/servicios/auth-fire.service.ts
@@ -0,0 +1,81 @@
+import { Injectable } from '@angular/core';
+import { Auth, signInWithPopup, GoogleAuthProvider, User, createUserWithEmailAndPassword  } from '@angular/fire/auth';
+import { signOut, sendPasswordResetEmail } from 'firebase/auth';
+import { BehaviorSubject } from 'rxjs';
+import { Firestore, collection, doc, getDoc, setDoc, getDocs, query, updateDoc, where } from '@angular/fire/firestore';
+import { docData } from 'rxfire/firestore';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthFireService {
+  private userSubject = new BehaviorSubject<User | null>(null);
+  user$ = this.userSubject.asObservable();
+
+  constructor(private auth: Auth, private firestore: Firestore){
+    this.auth.onAuthStateChanged(user => this.userSubject.next(user));
+  }
+
+  loginWithGoogle(){
+    const provider = new GoogleAuthProvider();
+    return signInWithPopup(this.auth, provider);
+  }
+
+  logout(){
+    return signOut(this.auth);
+  }
+
+  actualizarPassword(tipo: string, id: string, nuevaPassword: string): Promise<void> {
+    const coleccion = tipo === 'admin' ? 'admins' : 'usuarios';
+    const docRef = doc(this.firestore, `${coleccion}/${id}`);
+    return updateDoc(docRef, { password: nuevaPassword,   intentos: 0, bloq: false });
+  }
+
+
+  async obtenerDatosUsuario(tipo: string, username: string): Promise<any> {
+    const colPath = tipo === 'admin' ? 'admins' : 'usuarios';
+    const colRef = collection(this.firestore, colPath);
+    const q = query(colRef, where('nombre', '==', username));
+    const querySnapshot = await getDocs(q);
+
+    if (querySnapshot.empty) {
+      return null;
+    }
+
+    const docSnap = querySnapshot.docs[0];
+    return { id: docSnap.id, ...docSnap.data() };
+  }
+
+  async actualizarIntentosFallidos(tipo: string, id: string, intentos: number): Promise<void> {
+    const ruta = tipo === 'admin' ? `admins/${id}` : `usuarios/${id}`;
+    const docRef = doc(this.firestore, ruta);
+    await updateDoc(docRef, {
+      intentos: intentos
+    });
+  }
+
+  async bloquearCuenta(tipo: string, id: string): Promise<void> {
+    const ruta = tipo === 'admin' ? `admins/${id}` : `usuarios/${id}`;
+    const docRef = doc(this.firestore, ruta);
+    await updateDoc(docRef, {
+      bloq: true,
+      intentos: 3
+    });
+  }
+
+  async enviarCorreoRecuperacion(email: string): Promise<void> {
+    await sendPasswordResetEmail(this.auth, email);
+  }
+
+  async registrarUsuario(nombre: string, correo: string, password: string) {
+   const cred = await createUserWithEmailAndPassword(this.auth, correo, password);
+    const uid = cred.user.uid;
+    await setDoc(doc(this.firestore, 'usuarios', uid), {
+      nombre,
+      correo,
+      password,
+      intentos: 0,
+      bloq: false
+    });
+  }
+}
--- src/app/unete/unete.component.ts
+++ src/app/unete/unete.component.ts
@@ -4,10 +4,15 @@ import { RECAPTCHA_SETTINGS, RecaptchaFormsModule, RecaptchaModule, RecaptchaSet
 import { environment } from '../../environments/environment';
 import { FirestoreService } from '../servicios/firestore.service';
 import Swal from 'sweetalert2';
+import { AuthFireService } from '../servicios/auth-fire.service';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
+import { User } from 'firebase/auth';
 
 @Component({
   selector: 'app-unete',
-  imports: [ReactiveFormsModule, RecaptchaFormsModule, RecaptchaModule, FormsModule],
+  standalone: true,
+  imports: [ReactiveFormsModule, RecaptchaFormsModule, RecaptchaModule, FormsModule, AsyncPipe, CommonModule],
   providers: [
   {
     provide: RECAPTCHA_SETTINGS,
@@ -20,11 +25,13 @@ import Swal from 'sweetalert2';
   styleUrl: './unete.component.css'
 })
 
-export class UneteComponent {
+export class UneteComponent {  
   registroForm: FormGroup;
   token: string|undefined;
-
-  constructor(private fb: FormBuilder, private firestoreService: FirestoreService) {
+  user$!: Observable<User | null>;
+  
+  constructor(private fb: FormBuilder, private firestoreService: FirestoreService, private authService: AuthFireService) {
+    this.user$ = this.authService.user$;
     this.registroForm = this.fb.group({
       nombre: ["", [Validators.required, Validators.minLength(5)]],
       correo: ["", [Validators.required, this.emailValidator]],
@@ -34,7 +41,39 @@ export class UneteComponent {
 
     this.token = undefined;
   }
+  login() {
+  this.authService.loginWithGoogle().then(result => {
+    const user = result.user;
+    const datosUsuario = {
+      nombre: user.displayName ?? 'Sin nombre',
+      correo: user.email ?? 'Sin correo'
+    };
 
+    this.firestoreService.guardarUsuario(datosUsuario)
+      .then(() => {
+        console.log('Usuario con Google guardado en Firestore');
+        Swal.fire({
+          title: '¡Bienvenido con Google!',
+          text: `Hola ${user.displayName}`,
+          icon: 'success',
+          confirmButtonText: 'Aceptar',
+          confirmButtonColor: 'black',
+          color: 'black',
+          iconColor: 'black'
+        });
+      })
+      .catch(error => {
+        console.error('Error al guardar el usuario en Firestore:', error);
+      });
+  }).catch(err => {
+    console.error('Error en login con Google:', err);
+  });
+}
+
+
+  logout(){
+    this.authService.logout().catch(err => console.error(err));
+  }
   //Validar correo
   public emailValidator(control: FormControl): { [key: string]: boolean } | null {
     const emailRegexp: RegExp = /[@]/;
@@ -89,7 +128,6 @@ export class UneteComponent {
         this.registroForm.controls[control].markAsTouched();
       }
 
-      // Mostrar un mensaje de error para el captcha si es necesario
       if (!this.token) {
         console.warn('Captcha no completado');
       }
@@ -97,28 +135,33 @@ export class UneteComponent {
       return;
     }
 
-    this.firestoreService.guardarUsuario({
-      nombre: this.registroForm.get('nombre')?.value,
-      correo: this.registroForm.get('correo')?.value,
-      password: this.registroForm.get('password')?.value
-    }).then(() => {
-      console.log('Usuario guardado con éxito');
-    }).catch((err) => {
-      console.error('Error al guardar', err);
-    });
+    const nombre = this.registroForm.get('nombre')?.value;
+    const correo = this.registroForm.get('correo')?.value;
+    const password = this.registroForm.get('password')?.value;
 
-    Swal.fire({
-      title: 'Bienvenido a Gorilla Gym!',
-      icon: 'success',
-      confirmButtonText: 'Aceptar',
-      confirmButtonColor: 'black',
-      color: 'black',
-      iconColor: 'black'
-    })
+    this.authService.registrarUsuario(nombre, correo, password)
+      .then(() => {
+        Swal.fire({
+          title: 'Bienvenido a Gorilla Gym!',
+          icon: 'success',
+          confirmButtonText: 'Aceptar',
+          confirmButtonColor: 'black',
+          color: 'black',
+          iconColor: 'black'
+        });
 
-    this.registroForm.reset();
-    this.token = undefined;
-
-    console.debug(`Token [${this.token}] generated`);
+        this.registroForm.reset();
+        this.token = undefined;
+      })
+      .catch(error => {
+        console.error('Error al registrar usuario:', error);
+        Swal.fire({
+          title: 'Error',
+          text: error.message,
+          icon: 'error',
+          confirmButtonColor: 'black'
+        });
+      });
   }
+
 }
